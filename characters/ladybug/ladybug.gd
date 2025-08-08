93aa00c87897201a37f33c1ebec2c6032c3bf76d
eskelProgramming <166662972+eskelProgramming@users.noreply.github.com>
2025-08-08 14:40:33 -0700

    Movement comlpete

--- characters/ladybug/ladybug.gd
+++ characters/ladybug/ladybug.gd
@@ -1,32 +1,42 @@
-extends Node2D
+extends CharacterBody2D
 
-@onready var _animated_sprite = $CharacterBody2D/AnimatedSprite2D
+@onready var _animated_sprite = $AnimatedSprite2D
 
 var current_direction = Direction.DOWN
+var speed = 200.0
 
-func _process(_delta):
+	
+func _physics_process(delta: float) -> void:
+	velocity = Vector2.ZERO
+	
 	if Input.is_action_pressed("right"):
 		_animated_sprite.play("walk_right")
 		current_direction = Direction.RIGHT
-	elif Input.is_action_pressed("left"):
+		velocity.x += 1
+	if Input.is_action_pressed("left"):
 		_animated_sprite.play("walk_left")
 		current_direction = Direction.LEFT
-	elif  Input.is_action_pressed("down"):
+		velocity.x -= 1
+	if  Input.is_action_pressed("down"):
 		_animated_sprite.play("walk_down")
 		current_direction = Direction.DOWN
-	elif Input.is_action_pressed("up"):
+		velocity.y += 1
+	if Input.is_action_pressed("up"):
 		_animated_sprite.play("walk_up")
 		current_direction = Direction.UP
-	else:
-		match current_direction:
-			Direction.RIGHT:
-				_animated_sprite.play("idle_right")
-			Direction.LEFT:
-				_animated_sprite.play("idle_left")
-			Direction.DOWN:
-				_animated_sprite.play("idle_down")
-			Direction.UP:
-				_animated_sprite.play("idle_up")
+		velocity.y -= 1
+	
+	if Input.is_action_just_released("right"):
+		_animated_sprite.play("idle_right")
+	if Input.is_action_just_released("left"):
+		_animated_sprite.play("idle_left")
+	if Input.is_action_just_released("down"):
+		_animated_sprite.play("idle_down")
+	if Input.is_action_just_released("up"):
+		_animated_sprite.play("idle_up")
+	
+	velocity = velocity.normalized() * speed
+	move_and_slide()
 
 enum Direction {
 	UP, DOWN, RIGHT, LEFT
