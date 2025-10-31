29ea14bcec5521c76c9cbcaec4b45237d5004d3a
eskelProgramming <166662972+eskelProgramming@users.noreply.github.com>
2025-10-31 13:04:06 -0700

    Toolbar Fully Functional
    Toolbar is fully functional. 1-4 keys work correctly. Mouse selection also works.

--- characters/ladybug/ladybug.gd
+++ characters/ladybug/ladybug.gd
@@ -1,11 +1,13 @@
 extends CharacterBody2D
 
 @onready var _animated_sprite = $AnimatedSprite2D
+@onready var toolbar = $toolbar
 
 # Properties
 @export var speed = 250.0
 var face_direction = "down"
 var animation_to_play = "idle_down"
+var current_tool = Tools.WATERING_CAN
 
 # Start front idle animation on load
 func _ready():
@@ -33,10 +35,44 @@ func _physics_process(_delta):
 	if Input.is_action_pressed("down"):
 		velocity.y += 1.0 * speed
 		face_direction = "down"
-		
+	
 	# All movement animations named appropriately, eg "Left_Idle" or "Back_Walk"
 	animation_to_play = ("walk" if velocity.length() > 0.0 else "idle") + "_" + face_direction
 	_animated_sprite.play(animation_to_play)
 	
+	if Input.is_action_just_pressed("toolbar_1"):
+		current_tool = Tools.WATERING_CAN
+	if Input.is_action_just_pressed("toolbar_2"):
+		current_tool = Tools.HOE
+	if Input.is_action_just_pressed("toolbar_3"):
+		current_tool = Tools.SCYTHE
+	if Input.is_action_just_pressed("toolbar_4"):
+		current_tool = Tools.SEEDBAG
+	
 	# Move character, slide at collision
 	move_and_slide()
+	
+	# Set the toolbar to the correct tool
+	match current_tool:
+		Tools.WATERING_CAN:
+			toolbar.select(0)
+		Tools.HOE:
+			toolbar.select(1)
+		Tools.SCYTHE:
+			toolbar.select(2)
+		Tools.SEEDBAG:
+			toolbar.select(3)
+
+enum Tools { WATERING_CAN, HOE, SCYTHE, SEEDBAG }
+
+
+func _on_toolbar_item_clicked(index: int, at_position: Vector2, mouse_button_index: int) -> void:
+	match index:
+		0:
+			current_tool = Tools.WATERING_CAN
+		1:
+			current_tool = Tools.HOE
+		2:
+			current_tool = Tools.SCYTHE
+		3: 
+			current_tool = Tools.SEEDBAG
