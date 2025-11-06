3a268134ba72e3640ae138b7cf648ec9b0d90b84
eskelProgramming <166662972+eskelProgramming@users.noreply.github.com>
2025-11-06 14:14:54 -0800

    Fixed merge issue from last PR

--- tilesheets/dirt.gd
+++ tilesheets/dirt.gd
@@ -14,12 +14,22 @@ func hoe_dirt() :
 		update_state(dirt_states.TILLED)
 
 func water_dirt() :
-	if dirt_state == TILLED:
-		animation_player.play("watered")
-		dirt_state = WATERED
-	
+	if dirt_state == dirt_states.TILLED:
+		update_state(dirt_states.WATERED)
 
-enum {UNTILLED, TILLED, WATERED}
+func update_state(state):
+	match state:
+		dirt_states.UNTILLED:
+			animation_player.play("default")
+			dirt_state = dirt_states.UNTILLED
+		dirt_states.TILLED:
+			animation_player.play("tilled")
+			dirt_state = dirt_states.TILLED
+		dirt_states.WATERED:
+			animation_player.play("watered")
+			dirt_state = dirt_states.WATERED
+
+enum dirt_states {UNTILLED, TILLED, WATERED}
 
 func _on_mouse_entered() -> void:
 	MouseHandler.current_tile = self
