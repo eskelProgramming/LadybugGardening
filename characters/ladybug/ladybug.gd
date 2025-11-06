50b62f0aaf75f945a51427743068c8242565bb10
Emily Skelley <166662972+eskelProgramming@users.noreply.github.com>
2025-11-06 11:36:28 -0800

    Hoe logic #15 (#16)
    * Fixed and updated toolbar
    
    * Created Mouse Controls
    
    Not fully functional, but can now report which tile it is over
    
    * Tilling partially functional
    
    * Adjusted Camera zoom
    
    * Fixed tilling location logic. Tilling fully functional
    
    Tilling is only allowed within a certain distance to the player.

--- characters/ladybug/ladybug.gd
+++ characters/ladybug/ladybug.gd
@@ -1,13 +1,15 @@
+class_name Ladybug
+
 extends CharacterBody2D
 
 @onready var _animated_sprite = $AnimatedSprite2D
-@onready var toolbar = $toolbar
+@onready var toolbar = $Camera2D/toolbar
+@onready var ladybug = $"."
 
 # Properties
 @export var speed = 250.0
 var face_direction = "down"
 var animation_to_play = "idle_down"
-var current_tool = Tools.WATERING_CAN
 
 # Start front idle animation on load
 func _ready():
@@ -36,43 +38,61 @@ func _physics_process(_delta):
 		velocity.y += 1.0 * speed
 		face_direction = "down"
 	
-	# All movement animations named appropriately, eg "Left_Idle" or "Back_Walk"
+	if Input.is_action_just_pressed("toolbar_1"):
+		ToolManager.current_tool = ToolManager.Tools.WATERING_CAN
+	if Input.is_action_just_pressed("toolbar_2"):
+		ToolManager.current_tool = ToolManager.Tools.HOE
+	if Input.is_action_just_pressed("toolbar_3"):
+		ToolManager.current_tool = ToolManager.Tools.SCYTHE
+	if Input.is_action_just_pressed("toolbar_4"):
+		ToolManager.current_tool = ToolManager.Tools.SEEDBAG
+		
+		# Set the toolbar to the correct tool
+	match ToolManager.current_tool:
+		ToolManager.Tools.WATERING_CAN:
+			toolbar.select(0)
+		ToolManager.Tools.HOE:
+			toolbar.select(1)
+		ToolManager.Tools.SCYTHE:
+			toolbar.select(2)
+		ToolManager.Tools.SEEDBAG:
+			toolbar.select(3)
+	
 	animation_to_play = ("walk" if velocity.length() > 0.0 else "idle") + "_" + face_direction
+	
+	if Input.is_action_just_pressed("use") :
+		if MouseHandler.current_tile != null && is_ladybug_in_range() :
+			match ToolManager.current_tool:
+				ToolManager.Tools.WATERING_CAN:
+					pass
+				ToolManager.Tools.HOE:
+					MouseHandler.current_tile.hoe_dirt()
+					animation_to_play = "hoe_" + face_direction
+				ToolManager.Tools.SCYTHE:
+					pass
+				ToolManager.Tools.SEEDBAG:
+					pass
+	
 	_animated_sprite.play(animation_to_play)
 	
-	if Input.is_action_just_pressed("toolbar_1"):
-		current_tool = Tools.WATERING_CAN
-	if Input.is_action_just_pressed("toolbar_2"):
-		current_tool = Tools.HOE
-	if Input.is_action_just_pressed("toolbar_3"):
-		current_tool = Tools.SCYTHE
-	if Input.is_action_just_pressed("toolbar_4"):
-		current_tool = Tools.SEEDBAG
-	
-	# Move character, slide at collision
 	move_and_slide()
-	
-	# Set the toolbar to the correct tool
-	match current_tool:
-		Tools.WATERING_CAN:
-			toolbar.select(0)
-		Tools.HOE:
-			toolbar.select(1)
-		Tools.SCYTHE:
-			toolbar.select(2)
-		Tools.SEEDBAG:
-			toolbar.select(3)
-
-enum Tools { WATERING_CAN, HOE, SCYTHE, SEEDBAG }
 
 
 func _on_toolbar_item_clicked(index: int, at_position: Vector2, mouse_button_index: int) -> void:
 	match index:
 		0:
-			current_tool = Tools.WATERING_CAN
+			ToolManager.current_tool = ToolManager.Tools.WATERING_CAN
+			toolbar.select(0)
 		1:
-			current_tool = Tools.HOE
+			ToolManager.current_tool = ToolManager.Tools.HOE
+			toolbar.select(1)
 		2:
-			current_tool = Tools.SCYTHE
-		3: 
-			current_tool = Tools.SEEDBAG
+			ToolManager.current_tool = ToolManager.Tools.SCYTHE
+			toolbar.select(2)
+		3:
+			ToolManager.current_tool = ToolManager.Tools.SEEDBAG
+			toolbar.select(3)
+
+func is_ladybug_in_range():
+	var distance_to = ladybug.position.distance_to(MouseHandler.current_tile.position)
+	return distance_to < 40
